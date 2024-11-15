3348a7122ef7a655596ee35c11a9c6f225a51191
Radimir Gaysin <ragaysin@avito.ru>
2024-11-15 22:10:23 +0700

    feat: add support for non-stream and streaming chat completions
    - Removed `completions.http` file as it's no longer relevant.
    - Introduced `utils.py` with helper functions for handling async streaming and non-stream models.
    - Created `openai_api.http` with HTTP examples to test API endpoints.
    - Updated `main.py` to handle both streaming and non-streaming chat completions.
      - Added error handling to provide more clarity on API errors.
      - Modified the `/v1/chat/completions` endpoint to utilize streaming based on model capability.
      - Added a new endpoint `/v1/models` to list available models.

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import openai
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from openai import AsyncClient
+from openai.pagination import AsyncPage
+from openai.types import Model
 from openai.types.chat import ChatCompletion
-from openai.types.chat.completion_create_params import CompletionCreateParamsNonStreaming
+from openai.types.chat.completion_create_params import CompletionCreateParams
+from starlette.responses import StreamingResponse
 
 from settings import settings
+from utils import iter_async_stream, NON_STREAM_MODELS, handle_non_stream_models
 
 app = FastAPI()
 
@@ -25,12 +29,33 @@ async def handle_openai_api_errors(request: Request, call_next):
     try:
         return await call_next(request)
     except openai.APIError as e:
+        message = json.dumps(e.body) if e.body else e.message
+        status_code = e.status_code if hasattr(e, "status_code") else 400
+        print(status_code, type(e).__name__, message)
         return JSONResponse(
-            content=json.dumps(e.body) if e.body else e.message,
-            status_code=e.status_code if hasattr(e, 'status_code') else 400,
+            content=message,
+            status_code=status_code,
         )
 
 
-@app.post("/chat/completions")
-async def chat_completions(params: CompletionCreateParamsNonStreaming) -> ChatCompletion:
-    return await client.chat.completions.create(**params)
+@app.post("/v1/chat/completions", response_model=ChatCompletion)
+async def chat_completions(params: CompletionCreateParams):
+    stream_support = params['model'] not in NON_STREAM_MODELS
+    stream_requested = params.get('stream', False)
+    params['stream'] = stream_requested and stream_support
+    response = await client.chat.completions.create(**params)
+    if stream_requested:
+        if stream_support:
+            stream = iter_async_stream(response)
+        else:
+            stream = handle_non_stream_models(response)
+        return StreamingResponse(
+            content=stream,
+            media_type="text/event-stream; charset=utf-8",
+        )
+    return response
+
+
+@app.get("/v1/models")
+async def models() -> AsyncPage[Model]:
+    return await client.models.list()
---
+++ utils.py
@@ -0,0 +1,36 @@
+from collections.abc import AsyncGenerator
+
+from openai import AsyncStream
+from openai.types.chat import ChatCompletion, ChatCompletionChunk, chat_completion_chunk
+
+NON_STREAM_MODELS = {
+    'o1-preview',
+    'o1-mini',
+}
+
+
+async def iter_async_stream(stream: AsyncStream) -> AsyncGenerator[str, None]:
+    async for chunk in stream:
+        yield f'data: {chunk.model_dump_json()}\n\n'
+    yield 'data: [DONE]\n\n'
+
+
+def handle_non_stream_models(completion: ChatCompletion) -> AsyncGenerator[str, None]:
+    choice = completion.choices[0]
+    original_content = choice.message.content
+    for content in ["", original_content, None]:
+        chunk = ChatCompletionChunk(
+            **completion.model_dump(exclude={'choices', 'object'}),
+            object='chat.completion.chunk',
+            choices=[
+                chat_completion_chunk.Choice(
+                    **choice.model_dump(exclude={'message'}),
+                    delta=chat_completion_chunk.ChoiceDelta(
+                        **choice.message.model_dump(exclude={'audio', 'content'}),
+                        content=content,
+                    )
+                )
+            ]
+        )
+        yield f'data: {chunk.model_dump_json()}\n\n'
+    yield 'data: [DONE]\n\n'
